75a06f572effbc92fb3a3d2a338cc755da96e764
GrahamL03 <graham.long0310@gmail.com>
2025-01-25 18:05:14 -0500

    0.0.01

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
+import Hello from "@/app/components/hello";
 
 export default function Home() {
   console.log('What am I -- SERVER/CLIENT');
   return (
-    <h1 className ="text-3xl"> Welcome to Next.js</h1>
+    <> 
+      <h1 className ="text-3xl"> Welcome to Next.js</h1>
+      <Hello />
+    </>
   );
 }
